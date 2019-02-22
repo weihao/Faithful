febee143e1d28aa11904e72c5e4f5fd98d38357c
25 <31719556+25@users.noreply.github.com>
2019-02-22 18:59:43 -0500

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -4,3 +4,9 @@ sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum install jenkins
 sudo systemctl start jenkins
 systemctl status jenkins
+
+
+cd /usr/local/src
+sudo wget http://www-us.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
+sudo tar -xf apache-maven-3.5.4-bin.tar.gz
+sudo mv apache-maven-3.5.4/ apache-maven/
