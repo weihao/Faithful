96703860b68be746f17a5f29d2900c9e0ed717eb
Weihao <weihao.gao@outlook.com>
2020-04-15 19:47:34 -0400

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,33 +1,52 @@
-sudo apt-get install \
+sudo add-apt-repository contrib
+sudo add-apt-repository non-free
+sudo apt-get update
+
+# nvidia
+sudo apt -y install nvidia-driver
+
+
+sudo apt-get -y install \
+    ddclient \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg2 \
-    software-properties-common
+    software-properties-common \
+    iptables \
+    git \
+    python3 \
+    python3-pip \
+    default-jre \
+    default-jdk \
+    maven \
+    lshw \
+    npm \
+    sysvinit-utils \
+    xrdp \
+    htop \
 
-sudo apt-get install iptables
-sudo apt-get install git
-sudo apt-get install -y python3-pip
 
-sudo curl -sL https://deb.nodesource.com/setup_10.x | bash -
-sudo apt-get install -y nodejs
 
+# yarn
+npm install -g yarn
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 12
+nvm use 12
+# docker
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-
-sudo apt-get install default-jre
-sudo apt-get install default-jdk
-
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.$
-
-sudo docker pull jenkins/jenkins:lts
-
-docker run -d -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 jenkins/jenkins:lts
+sudo apt-get -y install \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
 
